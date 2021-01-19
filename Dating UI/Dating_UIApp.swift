6d8ed2bd2a643b68e03241051cee83d809a37ac4
Mahesh Prasad <mahesh223prasad@gmail.com>
2021-01-19 20:34:52 +0530

    Initial Commit

---
+++ Dating UI/Dating_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dating_UIApp.swift
+//  Dating UI
+//
+//  Created by Mahesh Prasad on 19/01/21.
+//
+
+import SwiftUI
+
+@main
+struct Dating_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
